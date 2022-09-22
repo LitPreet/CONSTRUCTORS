df5d7017676a63deaa061ae6e92eaa8d627517a6
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2022-09-22 22:18:19 +0530

    Create 1.cpp

---
+++ Parameterized constructor/1.cpp
@@ -0,0 +1,33 @@
+
+// CPP program to illustrate
+// parameterized constructors
+#include <iostream>
+using namespace std;
+ 
+class Test {
+private:
+    int x, y;
+ 
+public:
+    // Parameterized Constructor
+    Test(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
+    }
+ 
+    int getX() { return x; }
+    int getY() { return y; }
+};
+ 
+int main()
+{
+    // Constructor called
+    Test p1(10, 15);
+ 
+    // Access values assigned by constructor
+    cout << "p1.x = " << p1.getX()
+         << ", p1.y = " << p1.getY();
+ 
+    return 0;
+}
